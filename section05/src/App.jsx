5d36bc551837df7e2560bc32d3d963436f0a3f23
9aram <dlrkxks1028@naver.com>
2024-08-22 21:47:25 +0900

    component

---
+++ section05/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+const Header2 = ()=> {
+  return (
+   <header>
+     <h1>header</h1>
+   </header>
+  );
+}
+
+function Header(){
+   return (
+    <header>
+      <h1>header</h1>
+    </header>
+   )
+}
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>안뇽 리액트</h1>
+    </>
+  )
+}
+
+export default App
